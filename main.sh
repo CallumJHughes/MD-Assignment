ce7a9bd4eb3c5a3393747b71ceea83aae0c753db
Callum Hughes <callumhughes2000@live.co.uk>
2021-11-24 22:46:16 +0000

    Added new code to create production stage control file prod_.in and ensure it has correct values. Also added code to create directories and organise files so parent directory is not a mess.

--- main.sh
+++ main.sh
@@ -13,6 +13,7 @@ sed -i "" 's/0.0032/0.004/' equil_temp.in
 # Change value of skin
 sed -i "" 's/.1/.6/' equil_temp.in
 
+# Include 100 in your loop
 for i in 110 120 #130 140 150 160 170 180 190 200 210 220 230 240 250
 do
   # Set actual temperature to variable t e.g. 1.1 instead of 110
@@ -24,9 +25,48 @@ do
   # Changes the placeholder temp to current temperature
   sed -i "" 's/temp/'$t'/' equil_$i.in
 
-  # Run equillibrium stage using md3 with initial_coords_256 and equil_$i.in as input files
+  # Run equillibrium stage using md3 with initial_coords_256 as input and equil_$i.in as control
+  # equil_$i.out and equil_$i.tup will be the output files
+  ./md3 <equil_$i.in >equil_$i.tup
+
+  # Change output file in production stage control file prod_$i.in  
+  sed 's/equil_'$i'.out/prod_'$i'.out/' equil_$i.in > prod_$i.in
+
+  # Change input file in equil_$i,in for the production stage and save to a new input file
+  sed -i "" 's/initial_coords_256/equil_'$i'.out/' prod_$i.in
+
+  # Change line 7 in prod_$i.in to be -1 and save to new input file
+  # This is to keep total energy of the system constant rather than temperature.
+  # Therefore no velocity scaling
+  sed -i "" '7s/'$t'/-1/' prod_$i.in
+
   # Run production stage using md3 with equil_$i.out and prod_$i.in as input files
-  # Organise files into relevant folders 
+  # prod_$i.out and prod_$i.tup will be the output files
+  ./md3 <prod_$i.in >prod_$i.tup
+
+  # Make directories for equilibrium and production stage files
+  mkdir Equilibrium
+  mkdir Production
+
+  # Make Directories for .in .out .tup files in Equilibrium directory
+  mkdir Equilibrium/in
+  mkdir Equilibrium/out
+  mkdir Equilibrium/tup
+
+  # Make directories for .in .out .tup files in Production directory
+  mkdir Production/in
+  mkdir Production/out
+  mkdir Production/tup
+
+  # Organise equilibrium stage files into relevant directories
+  mv equil_$i.in Equilibrium/in
+  mv equil_$i.out Equilibrium/out
+  mv equil_$i.tup Equilibrium/tup
+
+  # Organise production stage files into relavant directories
+  mv prod_$i.in Production/in
+  mv prod_$i.out Production/out
+  mv prod_$i.tup Production/tup
 done
 
 exit 0
