da451f9de6778e78301bb26bffbbf334f2a4e160
Callum Hughes <callumhughes2000@live.co.uk>
2022-01-05 14:17:34 +0000

    Added comments to further explain logic of code

--- block_avg.f90
+++ block_avg.f90
@@ -4,7 +4,7 @@ program block_avg
 
   ! Work out standard deviation
 
-  N = 8192 ! Needs to be size of data file e.g 10000
+  N = 8192 ! Needs to be size of data file e.g 10000 (1*(2**13)=8192)
   
   dataFile = 'U_err_110.dat' ! Needs to be looped (will be looped in main script)
 
@@ -16,8 +16,12 @@ program block_avg
     do i = 1, N
       inputData(i) = blockData(i)
     end do
-    print *, size(blockData)
+    print *, 'Size of data set is: ', size(blockData)
     call MeanVariance
+    print * ! Prints empty line to space out the output values
+
+    ! If there are still data points to be blocked then the arrays are deallocated for the next loop
+    ! Else the loop is exited and the arrays contain the last set of data aquired.
     if (mod(N,2) .EQ. 0) then
       deallocate(blockData)
       deallocate(inputData)
