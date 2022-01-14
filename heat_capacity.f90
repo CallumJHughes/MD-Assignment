179cf4aa9c136c3c685d62b7d8c0ce61ca1f3f25
Callum Hughes <callumhughes2000@live.co.uk>
2022-01-14 23:42:25 +0000

    Added some comments for the next steps that need to be take to be taken

--- heat_capacity.f90
+++ heat_capacity.f90
@@ -2,6 +2,12 @@ program heat_capacity
   use stats
   implicit none
 
+  !!! READ LECTURE NOTES, SEE WHAT FLUCTUATION FORMUALA IS !!!
+
+  ! Import data file containing temperature values
+
+  ! Calculate mean value of temperature using block averaging (What are the benefits of block averaging???)
+
   ! Find mean of K (K=3/2*N*T) using block averages
 
   ! Find mean of K squared (K2=9/4*N2*T2) using block averages
