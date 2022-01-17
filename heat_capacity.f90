b927839095627a6c7edcc12f06cfe533d95cdf3d
Callum Hughes <callumhughes2000@live.co.uk>
2022-01-17 21:20:42 +0000

    Added code to find kinetic energy

--- heat_capacity.f90
+++ heat_capacity.f90
@@ -1,18 +1,26 @@
 program heat_capacity
   use stats
   implicit none
+  integer, parameter :: dp=selected_real_kind(15,300)
+  
+  real(kind=dp) :: kB = 1.380649*(10**-23)
+  real(kind=dp) :: meanTemp, meanK, meanK2
+  integer :: Natoms
+
+  Natoms = 256
 
   !!! READ LECTURE NOTES, SEE WHAT FLUCTUATION FORMUALA IS !!!
 
   ! Import data file containing temperature values
 
-  ! Calculate mean value of temperature using block averaging (What are the benefits of block averaging???)
-
   ! Find mean of K (K=3/2*N*T) using block averages
+  meanK = (3/2) * Natoms * kB * meanTemp
 
   ! Find mean of K squared (K2=9/4*N2*T2) using block averages
 
   ! Find average of delta K squared using formula
+
+  ! DELTA K IS VARIANCE LECTURE NOTES PAGE 41
 
   ! Calculate Cv using formula
 
